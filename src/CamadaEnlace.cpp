fc625767f6bfbfe86da5d4f258e59539434f679e
Smylle3 <joao21731@gmail.com>
2023-07-19 08:41:35 -0300

    Comenta código

--- src/CamadaEnlace.cpp
+++ src/CamadaEnlace.cpp
@@ -7,30 +7,12 @@ using namespace std;
 int tipoDeEnquadramento = 0;  // Alterar de acordo com o teste
 int tipoDeControleDeErro = 0; // Alterar de acordo com o teste
 
-bool potenciaDeDois(int number)
-{
-	return (number & (number - 1)) == 0;
-}
-
-int calculaParidadeBit(const vector<int> &quadro, int posicao_paridade)
-{
-	int bit_paridade = 0;
-
-	// Calcular o bit de paridade utilizando o esquema de paridade par
-	for (int i = posicao_paridade; i < quadro.size(); i += (posicao_paridade + 1))
-	{
-		bit_paridade ^= quadro[i];
-	}
-
-	return bit_paridade;
-}
-
 void CamadaEnlaceDadosTransmissora(vector<int> quadro)
 {
 	vector<int> quadro_enquadrado;
 
-	quadro_enquadrado = CamadaEnlaceDadosTransmissoraEnquadramento(quadro);
-	quadro_enquadrado = CamadaEnlaceDadosTransmissoraControleDeErro(quadro_enquadrado);
+	quadro_enquadrado = CamadaEnlaceDadosTransmissoraControleDeErro(quadro);
+	quadro_enquadrado = CamadaEnlaceDadosTransmissoraEnquadramento(quadro_enquadrado);
 
 	CamadaFisicaTransmissora(quadro_enquadrado);
 } // Fim da CamadaEnlaceDadosTransmissora
@@ -72,11 +54,16 @@ vector<int> CamadaDeEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(vec
 {
 	cout << "Enquadramento | Contagem de caracteres" << endl;
 
+	// Calcula a quantidade de bytes necessários para representar o quadro
 	uint8_t qtd_bytes = ceil(quadro.size() / 8);
+
+	// Cria uma cópia do quadro original para adicionar o enquadramento
 	vector<int> quadro_enquadrado_caracteres = quadro;
-	vector<int> binario;
+
+	// Converte a quantidade de bytes em uma representação binária de 8 bits
 	bitset<8> bits(qtd_bytes);
 
+	// Insere os bits da quantidade de bytes no início do quadro
 	for (int i = 0; i < 8; i++)
 	{
 		if (int(bits[i]) == 1)
@@ -88,29 +75,35 @@ vector<int> CamadaDeEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres(vec
 	cout << "Quadro enquadrado com contagem de caracteres: ";
 	ImprimeBits(quadro_enquadrado_caracteres);
 	return quadro_enquadrado_caracteres;
-} // Fim da CamadaDeEnlaceDadosTransmissoraEnquadramentoContagemDeCaracteres
+}
 
 vector<int> CamadaDeEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes(vector<int> quadro)
 {
 	cout << "Enquadramento | Insercao de bytes" << endl;
 
+	// Definição dos padrões utilizados
 	string flag = "00001111";
 	string esc = "11110000";
 	string flag_bit = "01111110";
+
 	string byte_str = "", quadro_str = flag;
 
 	vector<int> novo_quadro;
 	int counter = 1;
 
+	// Percorre cada bit do quadro original
 	for (int i = 0; i < quadro.size(); i++)
 	{
 		byte_str += to_string(quadro[i]);
 
+		// Verifica se é necessário inserir um byte completo
 		if (counter == (BYTE))
 		{
+			// Verifica se o byte formado é igual a algum dos padrões (flag ou escape)
 			if ((byte_str == flag) || (byte_str == esc))
 				quadro_str += esc;
 
+			// Insere o byte no quadro
 			quadro_str += byte_str;
 			counter = 0;
 			byte_str = "";
@@ -118,15 +111,17 @@ vector<int> CamadaDeEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes(vector<i
 		counter++;
 	}
 
+	// Adiciona a flag no final do quadro
 	quadro_str += flag;
 
+	// Converte o quadro para um vetor de inteiros
 	for (auto &i : quadro_str)
 		novo_quadro.push_back(i - '0');
 
 	cout << "Quadro enquadrado com insercao de bytes: ";
 	ImprimeBits(novo_quadro);
 	return novo_quadro;
-} // fim da CamadaDeEnlaceDadosTransmissoraEnquadramentoInsercaoDeBytes
+}
 
 vector<int> CamadaEnlaceDadosTransmissoraControleDeErroBitParidadePar(vector<int> quadro)
 {
@@ -158,30 +153,33 @@ vector<int> CamadaEnlaceDadosTransmissoraControleDeErroCRC(vector<int> quadro)
 
 	if (quadro.size() <= polinomio_crc_32.length())
 	{
-		cout << "Erro, o quadro possui menos bits que o polinomio" << endl;
+		cout << "Erro, o quadro possui menos bits que o polinômio" << endl;
 		exit(1);
 	}
 
+	// Adiciona zeros ao final do quadro para acomodar o polinômio CRC
 	for (int i = 0; i < polinomio_crc_32.length(); i++)
 		novo_quadro.push_back(0);
 
+	// Cálculo do CRC (Cyclic Redundancy Check)
 	for (int i = 0; i < quadro.size(); i++)
 	{
 		if (novo_quadro[i] == 1)
 		{
+			// Realiza a operação XOR entre o elemento i+j do novo quadro e o polinômio CRC
 			for (int j = 0; j < polinomio_crc_32.length(); j++)
 				novo_quadro[i + j] ^= (polinomio_crc_32[j] - '0');
-			// XOR entre o elemento i+j do novo quadro e o polinômio CRC 32bits; Armazena no próprio elemento i+j do novo quadro
 		}
 	}
 
+	// Restaura os bits originais do quadro
 	for (int i = 0; i < quadro.size(); i++)
 		novo_quadro[i] = quadro[i];
 
 	cout << "Controle CRC aplicado: ";
 	ImprimeBits(novo_quadro);
 	return novo_quadro;
-} // Fim da CamadaEnlaceDadosTransmissoraControleDeErroCRC
+}
 
 vector<int> CamadaEnlaceDadosTransmissoraControleDeErroCodigoDeHamming(vector<int> quadro)
 {
@@ -297,67 +295,78 @@ vector<int> CamadaDeEnlaceDadosReceptoraEnquadramentoContagemDeCaracteres(vector
 vector<int> CamadaDeEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes(vector<int> quadro)
 {
 	cout << "Camada De Enlace de dados Receptora | Enquadramento | Insercao de Bytes:" << endl;
-	string byte_str = "";
-	string quadro_str = "";
-	string flag = "00001111";
-	string esc = "11110000";
+	string byte_str = "";        // Variável para armazenar os bits de cada byte do quadro
+	string quadro_str = "";      // String para armazenar o quadro desenquadrado
+	string flag = "00001111";     // Byte de flag utilizado na inserção de bytes
+	string esc = "11110000";      // Byte de escape utilizado para escapar o byte de flag
 
-	vector<int> novo_quadro;
-	int counter = 1;
-	bool ignore = false;
+	vector<int> novo_quadro;     // Vetor para armazenar o quadro desenquadrado como inteiros
+	int counter = 1;             // Contador para acompanhar a formação de bytes
+	bool ignore = false;         // Flag para indicar se o byte atual deve ser ignorado
 
+	// Loop para percorrer os bits do quadro
 	for (int i = 0; i < quadro.size(); i++)
 	{
-		byte_str += to_string(quadro[i]);
+		byte_str += to_string(quadro[i]);   // Adiciona o bit atual à string do byte
+
+		// Verifica se um byte foi formado
 		if (counter == BYTE)
 		{
+			// Verifica se o byte é igual ao byte de flag ou de escape e não está sendo ignorado
 			if (((byte_str == flag) || (byte_str == esc)) && !ignore)
-				ignore = true;
+				ignore = true;   // Byte é marcado para ser ignorado
 			else
 			{
-				quadro_str += byte_str;
-				ignore = false;
+				quadro_str += byte_str;   // Byte é adicionado ao quadro desenquadrado
+				ignore = false;           // Reset da flag de ignorar
 			}
-			counter = 0;
-			byte_str = "";
+
+			counter = 0;        // Reset do contador de bits do byte
+			byte_str = "";     // Reset da string do byte
 		}
-		counter++;
+
+		counter++;   // Incrementa o contador de bits
 	}
+
+	// Converte o quadro desenquadrado em um vetor de inteiros
 	for (auto &i : quadro_str)
 		novo_quadro.push_back(i - '0');
 
 	cout << "Enquadramento por Insercao de bytes: ";
-	ImprimeBits(novo_quadro);
-	return novo_quadro;
-} // Fim da CamadaDeEnlaceDadosReceptoraEnquadramentoInsercaoDeBytes
+	ImprimeBits(novo_quadro);   // Imprime o quadro desenquadrado em forma de bits
+	return novo_quadro;         // Retorna o quadro desenquadrado
+}
 
 vector<int> CamadaEnlaceDadosReceptoraControleDeErroBitParidadePar(vector<int> quadro)
 {
 	cout << "Camada De Enlace de dados Receptora | Controle de erro | Paridade Par" << endl;
 
-	vector<int> recebimento_paridade_par;
-	bool paridade = true;
+	vector<int> recebimento_paridade_par;   // Vetor para armazenar o quadro desconsiderando o bit de paridade
+	bool paridade = true;                    // Variável para armazenar o cálculo da paridade
 
+	// Loop para percorrer os bits do quadro, desconsiderando o último bit de paridade
 	for (int i = 0; i < quadro.size() - 1; i++)
 		recebimento_paridade_par.push_back(quadro.at(i));
 
+	// Verifica a paridade dos bits do quadro desconsiderando o último bit de paridade
 	for (int i = 0; i < recebimento_paridade_par.size(); i++)
 		if (recebimento_paridade_par.at(i) == 1)
 			paridade = !paridade;
 
+	// Verifica se o último bit do quadro (bit de paridade) é igual ao cálculo da paridade
 	if (quadro.back() == paridade)
-		cout << "Recebeu com sucesso " << endl;
+		cout << "Recebeu com sucesso" << endl;
 
 	cout << "Controle por Paridade Par aplicado: ";
-	ImprimeBits(recebimento_paridade_par);
+	ImprimeBits(recebimento_paridade_par);   // Imprime o quadro desconsiderando o bit de paridade
 	return recebimento_paridade_par;
-} // Fim da CamadaEnlaceDadosReceptoraControleDeErroBitParidadePar
+}
 
 vector<int> CamadaEnlaceDadosReceptoraControleDeErroCRC(vector<int> quadro)
 {
 	cout << "Camada De Enlace de dados Receptora | Controle de erro | CRC" << endl;
 
-	string polinomio_crc_32 = "100110000010001110110110111";
+	string polinomio_crc_32 = "100110000010001110110110111";  // Polinômio CRC-32
 	vector<int> mensagem, novo_quadro;
 	bool valido = true;
 
@@ -369,32 +378,34 @@ vector<int> CamadaEnlaceDadosReceptoraControleDeErroCRC(vector<int> quadro)
 
 	novo_quadro = quadro;
 	mensagem = quadro;
-	mensagem.erase(mensagem.end() - 31, mensagem.end());
+	mensagem.erase(mensagem.end() - 31, mensagem.end());   // Remove os 32 bits de verificação do CRC do quadro recebido
 
+	// Realiza a verificação do CRC
 	for (int i = 0; i < mensagem.size(); i++)
 	{
 		if (quadro[i] == 1)
 		{
 			for (int j = 0; j < polinomio_crc_32.length(); j++)
 				novo_quadro[j + i] = novo_quadro[j + i] == polinomio_crc_32[j] ? 0 : 1;
-			/*  Compara o item i+j do novo quadro com o polinômio CRC 32 bits e armazena, no mesmo elemento,
-					0 se forem iguais, e 1 caso sejam diferentes. */
+			// Compara o item i+j do novo quadro com o polinômio CRC 32 bits e armazena, no mesmo elemento, 0 se forem iguais, e 1 caso sejam diferentes.
 		}
 	}
 
+	// Verifica se o quadro é válido, ou seja, se não houve detecção de erro no CRC
 	for (int i = 0; i < mensagem.size(); i++)
 	{
 		if (novo_quadro[i] != 0)
 			valido = false;
 	}
 
+	// Se o quadro for válido, imprime a mensagem original (sem os bits de verificação do CRC) e retorna como resultado
 	if (valido)
 	{
 		cout << "Controle por CRC aplicado: ";
 		ImprimeBits(mensagem);
 		return mensagem;
 	}
-} // Fim da CamadaEnlaceDadosReceptoraControleDeErroCRC
+}
 
 vector<int> CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming(vector<int> quadro)
 {
@@ -403,6 +414,7 @@ vector<int> CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming(vector<int> 
 	vector<int> decodificacao_hamming;
 	int posicao_paridade = 0;
 
+	// Percorre o quadro recebido, pulando as posições que correspondem aos bits de paridade
 	for (int i = 1; i <= quadro.size(); i++)
 	{
 		if (i == pow(2, posicao_paridade))
@@ -415,17 +427,18 @@ vector<int> CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming(vector<int> 
 		}
 	}
 
+	// Exibe o quadro decodificado após a remoção dos bits de paridade
 	cout << "Controle por Codigo de Hamming aplicado: ";
 	ImprimeBits(decodificacao_hamming);
 	return decodificacao_hamming;
-} // Fim da CamadaEnlaceDadosReceptoraControleDeErroCodigoDeHamming
+}
 
 void CamadaEnlaceDadosReceptora(vector<int> quadro)
 {
 	vector<int> quadro_desenquadrado;
 
-	quadro_desenquadrado = CamadaEnlaceDadosReceptoraControleDeErro(quadro);
-	quadro_desenquadrado = CamadaEnlaceDadosReceptoraEnquadramento(quadro_desenquadrado);
+	quadro_desenquadrado = CamadaEnlaceDadosReceptoraEnquadramento(quadro);
+	quadro_desenquadrado = CamadaEnlaceDadosReceptoraControleDeErro(quadro_desenquadrado);
 
 	CamadaDeAplicacaoReceptora(quadro_desenquadrado);
 } // Fim da CamadaEnlaceDadosReceptora
